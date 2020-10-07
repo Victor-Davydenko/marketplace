89c9a7f8ed6b4acc007989f889572691b20a66d1
Victor-Davydenko <59482820+Victor-Davydenko@users.noreply.github.com>
2020-10-08 01:21:49 +0300

    Updates

---
+++ precache-manifest.b129bd6387b8e36e12199dc989f343be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f436c7e95bace0acb2f59f925aaeacd",
+    "url": "/marketplace/index.html"
+  },
+  {
+    "revision": "fc1dcf6ccc5e90425353",
+    "url": "/marketplace/static/css/main.dd3a3a0b.chunk.css"
+  },
+  {
+    "revision": "da7ec6a78e9172285a19",
+    "url": "/marketplace/static/js/2.4c5c4fc7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/marketplace/static/js/2.4c5c4fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc1dcf6ccc5e90425353",
+    "url": "/marketplace/static/js/main.44a466af.chunk.js"
+  },
+  {
+    "revision": "7f0edb5975d9c2b53bb0",
+    "url": "/marketplace/static/js/runtime-main.b4440dba.js"
+  }
+]);
